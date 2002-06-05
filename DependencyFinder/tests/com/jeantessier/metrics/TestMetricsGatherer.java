f169088d7423fe8c87a529ef2cd748c71e058244
jeantessier <jeantessier@1c2012b4-aa10-0410-bfa9-9e87fac9e5c7>
2002-06-05 03:39:49 +0000

    Check additional measurements
    git-svn-id: https://depfind.svn.sourceforge.net/svnroot/depfind/trunk@122 1c2012b4-aa10-0410-bfa9-9e87fac9e5c7

--- DependencyFinder/tests/com/jeantessier/metrics/TestMetricsGatherer.java
+++ DependencyFinder/tests/com/jeantessier/metrics/TestMetricsGatherer.java
@@ -84,17 +84,55 @@ public class TestMetricsGatherer extends TestCase {
 	}
 	
 	public void test_test() {
-		assertEquals(Metrics.DEPTH_OF_INHERITANCE, 1, factory.CreateClassMetrics("test").Measurement(Metrics.DEPTH_OF_INHERITANCE).intValue());
+		assertEquals(Metrics.SLOC, 5, factory.CreateClassMetrics("test").Measurement(Metrics.SLOC).intValue());
 		assertEquals("M", 2, factory.CreateClassMetrics("test").Measurement("M").intValue());
 		assertEquals(Metrics.PUBLIC_METHODS, 2, factory.CreateClassMetrics("test").Measurement(Metrics.PUBLIC_METHODS).intValue());
 		assertEquals(Metrics.PROTECTED_METHODS, 0, factory.CreateClassMetrics("test").Measurement(Metrics.PROTECTED_METHODS).intValue());
 		assertEquals(Metrics.PRIVATE_METHODS, 0, factory.CreateClassMetrics("test").Measurement(Metrics.PRIVATE_METHODS).intValue());
 		assertEquals(Metrics.PACKAGE_METHODS, 0, factory.CreateClassMetrics("test").Measurement(Metrics.PACKAGE_METHODS).intValue());
+		assertEquals(Metrics.FINAL_METHODS, 0, factory.CreateClassMetrics("test").Measurement(Metrics.FINAL_METHODS).intValue());
+		assertEquals(Metrics.ABSTRACT_METHODS, 0, factory.CreateClassMetrics("test").Measurement(Metrics.ABSTRACT_METHODS).intValue());
+		assertEquals(Metrics.DEPRECATED_METHODS, 0, factory.CreateClassMetrics("test").Measurement(Metrics.DEPRECATED_METHODS).intValue());
 		assertEquals(Metrics.SYNTHETIC_METHODS, 0, factory.CreateClassMetrics("test").Measurement(Metrics.SYNTHETIC_METHODS).intValue());
 		assertEquals(Metrics.STATIC_METHODS, 1, factory.CreateClassMetrics("test").Measurement(Metrics.STATIC_METHODS).intValue());
+		assertEquals(Metrics.SYNCHRONIZED_METHODS, 0, factory.CreateClassMetrics("test").Measurement(Metrics.SYNCHRONIZED_METHODS).intValue());
+		assertEquals(Metrics.NATIVE_METHODS, 0, factory.CreateClassMetrics("test").Measurement(Metrics.NATIVE_METHODS).intValue());
+		assertEquals(Metrics.TRIVIAL_METHODS, 0, factory.CreateClassMetrics("test").Measurement(Metrics.TRIVIAL_METHODS).intValue());
 		assertEquals("PUBMR", 1, factory.CreateClassMetrics("test").Measurement("PUBMR").doubleValue(), 0.01);
+		assertEquals("PROMR", 0, factory.CreateClassMetrics("test").Measurement("PROMR").doubleValue(), 0.01);
+		assertEquals("PRIMR", 0, factory.CreateClassMetrics("test").Measurement("PRIMR").doubleValue(), 0.01);
+		assertEquals("PACMR", 0, factory.CreateClassMetrics("test").Measurement("PACMR").doubleValue(), 0.01);
+		assertEquals("FINMR", 0, factory.CreateClassMetrics("test").Measurement("FINMR").doubleValue(), 0.01);
+		assertEquals("ABSMR", 0, factory.CreateClassMetrics("test").Measurement("ABSMR").doubleValue(), 0.01);
+		assertEquals("DEPMR", 0, factory.CreateClassMetrics("test").Measurement("DEPMR").doubleValue(), 0.01);
+		assertEquals("SYNTHMR", 0, factory.CreateClassMetrics("test").Measurement("SYNTHMR").doubleValue(), 0.01);
 		assertEquals("STAMR", 0.5, factory.CreateClassMetrics("test").Measurement("STAMR").doubleValue(), 0.01);
-		assertEquals(Metrics.SLOC, 5, factory.CreateClassMetrics("test").Measurement(Metrics.SLOC).intValue());
+		assertEquals("SYNCHMR", 0, factory.CreateClassMetrics("test").Measurement("SYNCHMR").doubleValue(), 0.01);
+		assertEquals("NATMR", 0, factory.CreateClassMetrics("test").Measurement("NATMR").doubleValue(), 0.01);
+		assertEquals("TRIMR", 0, factory.CreateClassMetrics("test").Measurement("TRIMR").doubleValue(), 0.01);
+		assertEquals("A", 0, factory.CreateClassMetrics("test").Measurement("A").intValue());
+		assertEquals(Metrics.PUBLIC_ATTRIBUTES, 0, factory.CreateClassMetrics("test").Measurement(Metrics.PUBLIC_ATTRIBUTES).intValue());
+		assertEquals(Metrics.PROTECTED_ATTRIBUTES, 0, factory.CreateClassMetrics("test").Measurement(Metrics.PROTECTED_ATTRIBUTES).intValue());
+		assertEquals(Metrics.PRIVATE_ATTRIBUTES, 0, factory.CreateClassMetrics("test").Measurement(Metrics.PRIVATE_ATTRIBUTES).intValue());
+		assertEquals(Metrics.PACKAGE_ATTRIBUTES, 0, factory.CreateClassMetrics("test").Measurement(Metrics.PACKAGE_ATTRIBUTES).intValue());
+		assertEquals(Metrics.FINAL_ATTRIBUTES, 0, factory.CreateClassMetrics("test").Measurement(Metrics.FINAL_ATTRIBUTES).intValue());
+		assertEquals(Metrics.DEPRECATED_ATTRIBUTES, 0, factory.CreateClassMetrics("test").Measurement(Metrics.DEPRECATED_ATTRIBUTES).intValue());
+		assertEquals(Metrics.SYNTHETIC_ATTRIBUTES, 0, factory.CreateClassMetrics("test").Measurement(Metrics.SYNTHETIC_ATTRIBUTES).intValue());
+		assertEquals(Metrics.STATIC_ATTRIBUTES, 0, factory.CreateClassMetrics("test").Measurement(Metrics.STATIC_ATTRIBUTES).intValue());
+		assertEquals(Metrics.TRANSIENT_ATTRIBUTES, 0, factory.CreateClassMetrics("test").Measurement(Metrics.TRANSIENT_ATTRIBUTES).intValue());
+		assertEquals(Metrics.VOLATILE_ATTRIBUTES, 0, factory.CreateClassMetrics("test").Measurement(Metrics.VOLATILE_ATTRIBUTES).intValue());
+		assertTrue("PUBAR", Double.isNaN(factory.CreateClassMetrics("test").Measurement("PUBAR").doubleValue()));
+		assertTrue("PROAR", Double.isNaN(factory.CreateClassMetrics("test").Measurement("PROAR").doubleValue()));
+		assertTrue("PRIAR", Double.isNaN(factory.CreateClassMetrics("test").Measurement("PRIAR").doubleValue()));
+		assertTrue("PACAR", Double.isNaN(factory.CreateClassMetrics("test").Measurement("PACAR").doubleValue()));
+		assertTrue("FINAR", Double.isNaN(factory.CreateClassMetrics("test").Measurement("FINAR").doubleValue()));
+		assertTrue("DEPAR", Double.isNaN(factory.CreateClassMetrics("test").Measurement("DEPAR").doubleValue()));
+		assertTrue("SYNTHAR", Double.isNaN(factory.CreateClassMetrics("test").Measurement("SYNTHAR").doubleValue()));
+		assertTrue("STAAR", Double.isNaN(factory.CreateClassMetrics("test").Measurement("STAAR").doubleValue()));
+		assertTrue("TRAAR", Double.isNaN(factory.CreateClassMetrics("test").Measurement("TRAAR").doubleValue()));
+		assertTrue("VOLAR", Double.isNaN(factory.CreateClassMetrics("test").Measurement("VOLAR").doubleValue()));
+		assertEquals(Metrics.SUBCLASSES, 0, factory.CreateClassMetrics("test").Measurement(Metrics.SUBCLASSES).intValue());
+		assertEquals(Metrics.DEPTH_OF_INHERITANCE, 1, factory.CreateClassMetrics("test").Measurement(Metrics.DEPTH_OF_INHERITANCE).intValue());
 	}
 
 	public void test_() {
