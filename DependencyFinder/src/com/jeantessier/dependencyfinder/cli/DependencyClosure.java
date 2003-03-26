c2977396dfd44f432982d775fd5fa427f956efe1
jeantessier <jeantessier@1c2012b4-aa10-0410-bfa9-9e87fac9e5c7>
2003-03-26 07:45:19 +0000

    Clean up
    git-svn-id: https://depfind.svn.sourceforge.net/svnroot/depfind/trunk@396 1c2012b4-aa10-0410-bfa9-9e87fac9e5c7

--- DependencyFinder/src/com/jeantessier/dependencyfinder/cli/DependencyClosure.java
+++ DependencyFinder/src/com/jeantessier/dependencyfinder/cli/DependencyClosure.java
@@ -46,22 +46,6 @@ public class DependencyClosure {
     public static final String DEFAULT_FILTER_INCLUDES        = "//";
     public static final String DEFAULT_LOGFILE                = "System.out";
 
-	private static final Layout DEFAULT_LOG_LAYOUT = new PatternLayout("[%d{yyyy/MM/dd HH:mm:ss.SSS}] %c %m%n");
-
-	public static void Log(Logger logger, String filename) throws IOException {
-		Log(logger, filename, Level.DEBUG);
-	}
-	
-	public static void Log(Logger logger, String filename, Level level) throws IOException {
-		logger.setLevel(level);
-			
-		if ("System.out".equals(filename)) {
-			logger.addAppender(new ConsoleAppender(DEFAULT_LOG_LAYOUT));
-		} else {
-			logger.addAppender(new WriterAppender(DEFAULT_LOG_LAYOUT, new FileWriter(filename)));
-		}
-	}
-
     public static void Error(CommandLineUsage clu, String msg) {
 		System.err.println(msg);
 		Error(clu);
