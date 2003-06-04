67ee94b185e8936ad61d85f0b35975e31cf7caa9
jeantessier <jeantessier@1c2012b4-aa10-0410-bfa9-9e87fac9e5c7>
2003-06-04 00:32:26 +0000

    More quotes
    git-svn-id: https://depfind.svn.sourceforge.net/svnroot/depfind/trunk@450 1c2012b4-aa10-0410-bfa9-9e87fac9e5c7

--- DependencyFinder/bin/ClassClassDiff.bat
+++ DependencyFinder/bin/ClassClassDiff.bat
@@ -65,7 +65,7 @@ goto setupArgs
 rem This label provides a place for the argument list loop to break out
 rem and for NT handling to skip to.
 
-%JAVA_HOME%\bin\java %DEPENDENCYFINDER_OPTS% -classpath "%DEPENDENCYFINDER_HOME%\classes;%DEPENDENCYFINDER_HOME%\lib\DependencyFinder.jar;%DEPENDENCYFINDER_HOME%\lib\log4j.jar;%DEPENDENCYFINDER_HOME%\lib\jakarta-oro.jar" com.jeantessier.dependencyfinder.cli.ClassClassDiff %DEPENDENCYFINDER_CMD_LINE_ARGS%
+"%JAVA_HOME%\bin\java" %DEPENDENCYFINDER_OPTS% -classpath "%DEPENDENCYFINDER_HOME%\classes;%DEPENDENCYFINDER_HOME%\lib\DependencyFinder.jar;%DEPENDENCYFINDER_HOME%\lib\log4j.jar;%DEPENDENCYFINDER_HOME%\lib\jakarta-oro.jar" com.jeantessier.dependencyfinder.cli.ClassClassDiff %DEPENDENCYFINDER_CMD_LINE_ARGS%
 
 if not "%OS%"=="Windows_NT" goto mainEnd
 :winNTend
