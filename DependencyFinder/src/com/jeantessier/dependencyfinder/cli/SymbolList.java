226c0a77f0a04f3874f3eddc9c09f9e343ea3023
jeantessier <jeantessier@1c2012b4-aa10-0410-bfa9-9e87fac9e5c7>
2004-09-08 07:55:21 +0000

    New symbol list tool
    git-svn-id: https://depfind.svn.sourceforge.net/svnroot/depfind/trunk@715 1c2012b4-aa10-0410-bfa9-9e87fac9e5c7

---
+++ DependencyFinder/src/com/jeantessier/dependencyfinder/cli/SymbolList.java
@@ -0,0 +1,169 @@
+/*
+ *  Copyright (c) 2001-2004, Jean Tessier
+ *  All rights reserved.
+ *  
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *  
+ *  	* Redistributions of source code must retain the above copyright
+ *  	  notice, this list of conditions and the following disclaimer.
+ *  
+ *  	* Redistributions in binary form must reproduce the above copyright
+ *  	  notice, this list of conditions and the following disclaimer in the
+ *  	  documentation and/or other materials provided with the distribution.
+ *  
+ *  	* Neither the name of Jean Tessier nor the names of his contributors
+ *  	  may be used to endorse or promote products derived from this software
+ *  	  without specific prior written permission.
+ *  
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ *  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ *  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.jeantessier.dependencyfinder.cli;
+
+import java.io.*;
+import java.util.*;
+
+import org.apache.log4j.*;
+
+import com.jeantessier.classreader.*;
+import com.jeantessier.commandline.*;
+import com.jeantessier.dependencyfinder.*;
+
+public class SymbolList {
+	public static final String DEFAULT_LOGFILE = "System.out";
+
+	public static void showError(CommandLineUsage clu, String msg) {
+		System.err.println(msg);
+		showError(clu);
+	}
+
+	public static void showError(CommandLineUsage clu) {
+		System.err.println(clu);
+		System.err.println();
+		System.err.println("If no files are specified, it processes the current directory.");
+		System.err.println();
+	}
+
+	public static void showVersion() throws IOException {
+		Version version = new Version();
+		
+		System.err.print(version.getImplementationTitle());
+		System.err.print(" ");
+		System.err.print(version.getImplementationVersion());
+		System.err.print(" (c) ");
+		System.err.print(version.getCopyrightDate());
+		System.err.print(" ");
+		System.err.print(version.getCopyrightHolder());
+		System.err.println();
+		
+		System.err.print(version.getImplementationURL());
+		System.err.println();
+		
+		System.err.print("Compiled on ");
+		System.err.print(version.getImplementationDate());
+		System.err.println();
+	}
+
+	public static void main(String[] args) throws Exception {
+		// Parsing the command line
+		CommandLine commandLine = new CommandLine();
+		commandLine.addToggleSwitch("time");
+		commandLine.addSingleValueSwitch("out");
+		commandLine.addToggleSwitch("help");
+		commandLine.addOptionalValueSwitch("verbose", DEFAULT_LOGFILE);
+		commandLine.addToggleSwitch("version");
+
+		CommandLineUsage usage = new CommandLineUsage("SymbolList");
+		commandLine.accept(usage);
+
+		try {
+			commandLine.parse(args);
+		} catch (IllegalArgumentException ex) {
+			showError(usage, ex.toString());
+			System.exit(1);
+		} catch (CommandLineException ex) {
+			showError(usage, ex.toString());
+			System.exit(1);
+		}
+
+		if (commandLine.getToggleSwitch("help")) {
+			showError(usage);
+		}
+		
+		if (commandLine.getToggleSwitch("version")) {
+			showVersion();
+		}
+
+		if (commandLine.getToggleSwitch("help") || commandLine.getToggleSwitch("version")) {
+			System.exit(1);
+		}
+
+		VerboseListener verboseListener = new VerboseListener();
+		if (commandLine.isPresent("verbose")) {
+			if ("System.out".equals(commandLine.getOptionalSwitch("verbose"))) {
+				verboseListener.getWriter(System.out);
+			} else {
+				verboseListener.getWriter(new FileWriter(commandLine.getOptionalSwitch("verbose")));
+			}
+		}
+
+		/*
+		 *  Beginning of main processing
+		 */
+
+		Date start = new Date();
+
+		PrintWriter out;
+		if (commandLine.isPresent("out")) {
+			out = new PrintWriter(new FileWriter(commandLine.getSingleSwitch("out")));
+		} else {
+			out = new PrintWriter(new OutputStreamWriter(System.out));
+		}
+
+		List parameters = commandLine.getParameters();
+		if (parameters.size() == 0) {
+			parameters.add(".");
+		}
+
+		Collector collector = new SymbolGatherer();
+		
+		Iterator i;
+
+		i = parameters.iterator();
+		while (i.hasNext()) {
+			String filename = (String) i.next();
+
+			ClassfileLoader loader = new TransientClassfileLoader();
+			loader.addLoadListener(collector);
+			loader.addLoadListener(verboseListener);
+			loader.load(Collections.singleton(filename));
+		}
+
+		i = collector.getCollection().iterator();
+		while (i.hasNext()) {
+			out.println(i.next());
+		}
+
+		Date end = new Date();
+
+		if (commandLine.getToggleSwitch("time")) {
+			System.err.println(SymbolList.class.getName() + ": " + ((end.getTime() - (double) start.getTime()) / 1000) + " secs.");
+		}
+
+		out.close();
+
+		verboseListener.close();
+	}
+}
---
+++ DependencyFinder/tests/com/jeantessier/classreader/TestSymbolGatherer.java
@@ -0,0 +1,73 @@
+/*
+ *  Copyright (c) 2001-2004, Jean Tessier
+ *  All rights reserved.
+ *  
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *  
+ *  	* Redistributions of source code must retain the above copyright
+ *  	  notice, this list of conditions and the following disclaimer.
+ *  
+ *  	* Redistributions in binary form must reproduce the above copyright
+ *  	  notice, this list of conditions and the following disclaimer in the
+ *  	  documentation and/or other materials provided with the distribution.
+ *  
+ *  	* Neither the name of Jean Tessier nor the names of his contributors
+ *  	  may be used to endorse or promote products derived from this software
+ *  	  without specific prior written permission.
+ *  
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ *  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ *  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.jeantessier.classreader;
+
+import java.io.*;
+import java.util.*;
+
+import junit.framework.*;
+
+import org.apache.log4j.*;
+
+public class TestSymbolGatherer extends TestCase {
+	public static final String TEST_CLASS    = "test";
+	public static final String TEST_FILENAME = "classes" + File.separator + "test.class";
+	
+	private SymbolGatherer gatherer;
+	private ClassfileLoader loader;
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		gatherer = new SymbolGatherer();
+		loader   = new AggregatingClassfileLoader();
+		loader.addLoadListener(gatherer);
+	}
+
+	public void testEmpty() {
+		assertEquals("Different number of symbols", 0, gatherer.getCollection().size());
+	}
+	
+	public void testOnOneClass() throws IOException {
+		loader.load(Collections.singleton(TEST_FILENAME));
+
+		assertTrue("Missing test class name from " + gatherer.getCollection(), gatherer.getCollection().contains("test"));
+		assertTrue("Missing test.main() method from " + gatherer.getCollection(), gatherer.getCollection().contains("test.main(java.lang.String[])"));
+		assertTrue("Missing args parameter from " + gatherer.getCollection(), gatherer.getCollection().contains("test.main(java.lang.String[]): args"));
+		assertTrue("Missing c local variable from " + gatherer.getCollection(), gatherer.getCollection().contains("test.main(java.lang.String[]): c"));
+		assertTrue("Missing ex local variable from " + gatherer.getCollection(), gatherer.getCollection().contains("test.main(java.lang.String[]): ex"));
+		assertTrue("Missing test.test() method from " + gatherer.getCollection(), gatherer.getCollection().contains("test.test()"));
+		assertTrue("Missing this parameter from " + gatherer.getCollection(), gatherer.getCollection().contains("test.test(): this"));
+		assertEquals("Different number of symbols in " + gatherer.getCollection(), 7, gatherer.getCollection().size());
+	}
+}
