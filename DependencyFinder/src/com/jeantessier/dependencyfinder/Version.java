aff43825ee95c297a49f29d50c6bf33c24580ff6
jeantessier <jeantessier@1c2012b4-aa10-0410-bfa9-9e87fac9e5c7>
2006-01-20 05:21:37 +0000

    Update copyright notice for 2006
    git-svn-id: https://depfind.svn.sourceforge.net/svnroot/depfind/trunk@1103 1c2012b4-aa10-0410-bfa9-9e87fac9e5c7

--- DependencyFinder/src/com/jeantessier/dependencyfinder/Version.java
+++ DependencyFinder/src/com/jeantessier/dependencyfinder/Version.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2001-2005, Jean Tessier
+ *  Copyright (c) 2001-2006, Jean Tessier
  *  All rights reserved.
  *  
  *  Redistribution and use in source and binary forms, with or without
@@ -44,7 +44,7 @@ public class Version {
     public static final String DEFAULT_VENDOR           = "Jean Tessier";
     public static final String DEFAULT_DATE             = "<i>unknown</i>";
     public static final String DEFAULT_COPYRIGHT_HOLDER = "Jean Tessier";
-    public static final String DEFAULT_COPYRIGHT_DATE   = "2001-2005";
+    public static final String DEFAULT_COPYRIGHT_DATE   = "2001-2006";
 
     private String resourceURL = null;
     private String jarName     = null;
