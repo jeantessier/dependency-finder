b88f0e45d8a50ce626ec816cda728d143396a095
jeantessier <jeantessier@1c2012b4-aa10-0410-bfa9-9e87fac9e5c7>
2006-04-20 19:09:40 +0000

    Test data for method name length
    git-svn-id: https://depfind.svn.sourceforge.net/svnroot/depfind/trunk@1197 1c2012b4-aa10-0410-bfa9-9e87fac9e5c7

---
+++ DependencyFinder/tests/Metrics/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public void onewordmethodname() {}
+    public void twowordsMethodname() {}
+    public void threeWordsMethodname() {}
+    public void methodNameWithEndingTLA() {}
+    public void tlaAtTheBeginingOfMethodName() {}
+    public void methodNameWithTLAInTheMiddle() {}
+}
