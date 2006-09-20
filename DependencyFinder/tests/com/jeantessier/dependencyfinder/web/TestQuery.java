92a4b0c98acb8e63da06430d0392fe08c6f74f09
jeantessier <jeantessier@1c2012b4-aa10-0410-bfa9-9e87fac9e5c7>
2006-09-20 22:58:04 +0000

    Successful submit with ServletUnit
    git-svn-id: https://depfind.svn.sourceforge.net/svnroot/depfind/trunk@1242 1c2012b4-aa10-0410-bfa9-9e87fac9e5c7

--- DependencyFinder/tests/com/jeantessier/dependencyfinder/web/TestQuery.java
+++ DependencyFinder/tests/com/jeantessier/dependencyfinder/web/TestQuery.java
@@ -1,6 +1,7 @@
 package com.jeantessier.dependencyfinder.web;
 
 import java.io.*;
+import java.util.*;
 
 import com.meterware.httpunit.*;
 import com.meterware.servletunit.*;
@@ -12,9 +13,9 @@ public class TestQuery extends TestCase {
     private static final String NO_GRAPH_MESSAGE = "There is no dependency graph at this time.";
 
     private NodeFactory factory;
-    FeatureNode a_A_a;
-    FeatureNode b_B_b;
-    FeatureNode c_C_c;
+    FeatureNode foo;
+    FeatureNode bar;
+    FeatureNode baz;
 
     private ServletUnitClient client;
     private WebRequest request;
@@ -25,10 +26,12 @@ public class TestQuery extends TestCase {
 
         Logger.getLogger(getClass()).setLevel(Level.ALL);
 
+        Random random = new Random();
+
         factory = new NodeFactory();
-        a_A_a = factory.createFeature("a.A.a");
-        b_B_b = factory.createFeature("b.B.b");
-        c_C_c = factory.createFeature("c.C.c");
+        foo = factory.createFeature("foo" + random.nextLong() + ".Foo" + random.nextLong() + ".foo" + random.nextLong());
+        bar = factory.createFeature("bar" + random.nextLong() + ".Bar" + random.nextLong() + ".bar" + random.nextLong());
+        baz = factory.createFeature("baz" + random.nextLong() + ".Baz" + random.nextLong() + ".baz" + random.nextLong());
 
         ServletRunner runner = new ServletRunner(new File("web/WEB-INF/web.xml"), "/web");
         client = runner.newClient();
@@ -38,7 +41,7 @@ public class TestQuery extends TestCase {
 
     public void testNoDependencyGraph() throws Exception {
         context.service();
-        WebResponse response = context.getServletResponse();
+        WebResponse response = client.getResponse(request);
         assertTrue("Missing text \"" + NO_GRAPH_MESSAGE + "\"", response.getText().contains(NO_GRAPH_MESSAGE));
     }
 
@@ -46,35 +49,74 @@ public class TestQuery extends TestCase {
         client.getSession(true).getServletContext().setAttribute("factory", factory);
 
         context.service();
-        WebResponse response = context.getServletResponse();
+        WebResponse response = client.getResponse(request);
         assertFalse("Unexpected text \"" + NO_GRAPH_MESSAGE + "\"", response.getText().contains(NO_GRAPH_MESSAGE));
     }
 
     public void testTestDependencyGraph() throws Exception {
-        a_A_a.addDependency(b_B_b);
-        b_B_b.addDependency(c_C_c);
+        foo.addDependency(bar);
+        bar.addDependency(baz);
 
         client.getSession(true).getServletContext().setAttribute("factory", factory);
 
         context.service();
-        WebResponse response = context.getServletResponse();
+        WebResponse response = client.getResponse(request);
         assertFalse("Unexpected text \"" + NO_GRAPH_MESSAGE + "\"", response.getText().contains(NO_GRAPH_MESSAGE));
     }
 
-    public void testQuery() throws Exception {
-        a_A_a.addDependency(b_B_b);
-        b_B_b.addDependency(c_C_c);
+    public void testFormSubmit() throws Exception {
+        foo.addDependency(bar);
+        bar.addDependency(baz);
 
         client.getSession(true).getServletContext().setAttribute("factory", factory);
 
         context.service();
-        WebResponse response = context.getServletResponse();
-        Logger.getLogger(getClass()).debug(response.getText());
-        assertFalse("Unexpected text \"" + NO_GRAPH_MESSAGE + "\"", response.getText().contains(NO_GRAPH_MESSAGE));
+        WebResponse response = client.getResponse(request);
 
-        response.getForms()[0].getSubmitButton("submit").click();
-        Logger.getLogger(getClass()).debug(response.getText());
-        String a2b = "a --&gt; b";
-        assertFalse("Missing text \"" + a2b + "\"", response.getText().contains(a2b));
+        assertEquals("Nb forms", 1, response.getForms().length);
+        WebForm form = response.getForms()[0];
+        assertEquals("Nb submit buttons", 1, form.getSubmitButtons().length);
+        SubmitButton button = form.getSubmitButtons()[0];
+
+        response = form.submit(button);
+
+        assertNotNull("Missing link to " + foo.getClassNode().getPackageNode().getName(), response.getLinkWith(foo.getClassNode().getPackageNode().getName()));
+        assertNull("Unwanted link to " + foo.getClassNode().getName(), response.getLinkWith(foo.getClassNode().getName()));
+        assertNull("Unwanted link to " + foo.getName(), response.getLinkWith(foo.getName()));
+        assertNotNull("Missing link to " + bar.getClassNode().getPackageNode().getName(), response.getLinkWith(bar.getClassNode().getPackageNode().getName()));
+        assertNull("Unwanted link to " + bar.getClassNode().getName(), response.getLinkWith(bar.getClassNode().getName()));
+        assertNull("Unwanted link to " + bar.getName(), response.getLinkWith(bar.getName()));
+        assertNotNull("Missing link to " + baz.getClassNode().getPackageNode().getName(), response.getLinkWith(baz.getClassNode().getPackageNode().getName()));
+        assertNull("Unwanted link to " + baz.getClassNode().getName(), response.getLinkWith(baz.getClassNode().getName()));
+        assertNull("Unwanted link to " + baz.getName(), response.getLinkWith(baz.getName()));
+    }
+
+    public void testQuery() throws Exception {
+        foo.addDependency(bar);
+        bar.addDependency(baz);
+
+        client.getSession(true).getServletContext().setAttribute("factory", factory);
+
+        request.setParameter("scope-includes", "//");
+        request.setParameter("package-scope", "on");
+        request.setParameter("filter-includes", "//");
+        request.setParameter("package-filter", "on");
+        request.setParameter("show-inbounds", "on");
+        request.setParameter("show-outbounds", "on");
+        request.setParameter("show-empty-nodes", "on");
+        request.setParameter("submit", "Run Query");
+
+        context.service();
+        WebResponse response = client.getResponse(request);
+
+        assertNotNull("Missing link to " + foo.getClassNode().getPackageNode().getName(), response.getLinkWith(foo.getClassNode().getPackageNode().getName()));
+        assertNull("Unwanted link to " + foo.getClassNode().getName(), response.getLinkWith(foo.getClassNode().getName()));
+        assertNull("Unwanted link to " + foo.getName(), response.getLinkWith(foo.getName()));
+        assertNotNull("Missing link to " + bar.getClassNode().getPackageNode().getName(), response.getLinkWith(bar.getClassNode().getPackageNode().getName()));
+        assertNull("Unwanted link to " + bar.getClassNode().getName(), response.getLinkWith(bar.getClassNode().getName()));
+        assertNull("Unwanted link to " + bar.getName(), response.getLinkWith(bar.getName()));
+        assertNotNull("Missing link to " + baz.getClassNode().getPackageNode().getName(), response.getLinkWith(baz.getClassNode().getPackageNode().getName()));
+        assertNull("Unwanted link to " + baz.getClassNode().getName(), response.getLinkWith(baz.getClassNode().getName()));
+        assertNull("Unwanted link to " + baz.getName(), response.getLinkWith(baz.getName()));
     }
 }
