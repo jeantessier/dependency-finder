4b90c071098bf42f49ae61b06329afc43562e233
jeantessier <jeantessier@1c2012b4-aa10-0410-bfa9-9e87fac9e5c7>
2008-10-30 23:26:11 +0000

    Enum variety of element value
    git-svn-id: https://depfind.svn.sourceforge.net/svnroot/depfind/trunk@1795 1c2012b4-aa10-0410-bfa9-9e87fac9e5c7

--- DependencyFinder/src/com/jeantessier/classreader/impl/EnumElementValue.java
+++ DependencyFinder/src/com/jeantessier/classreader/impl/EnumElementValue.java
@@ -1,22 +1,22 @@
 /*
  *  Copyright (c) 2001-2008, Jean Tessier
  *  All rights reserved.
- *  
+ *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions
  *  are met:
- *  
+ *
  *      * Redistributions of source code must retain the above copyright
  *        notice, this list of conditions and the following disclaimer.
- *  
+ *
  *      * Redistributions in binary form must reproduce the above copyright
  *        notice, this list of conditions and the following disclaimer in the
  *        documentation and/or other materials provided with the distribution.
- *  
+ *
  *      * Neither the name of Jean Tessier nor the names of his contributors
  *        may be used to endorse or promote products derived from this software
  *        without specific prior written permission.
- *  
+ *
  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -32,32 +32,56 @@
 
 package com.jeantessier.classreader.impl;
 
-import junit.framework.*;
+import java.io.*;
 
-public class TestAll extends TestCase {
-    public static Test suite() {
-        TestSuite result = new TestSuite(TestAll.class.getPackage().getName());
+import org.apache.log4j.*;
 
-        result.addTestSuite(TestInstruction.class);
-        result.addTestSuite(TestInstructionWithConstantPool.class);
-        result.addTestSuite(TestDeprecated_attribute.class);
-        result.addTestSuite(TestByteConstantElementValue.class);
-        result.addTestSuite(TestCharConstantElementValue.class);
-        result.addTestSuite(TestDoubleConstantElementValue.class);
-        result.addTestSuite(TestFloatConstantElementValue.class);
-        result.addTestSuite(TestIntegerConstantElementValue.class);
-        result.addTestSuite(TestLongConstantElementValue.class);
-        result.addTestSuite(TestShortConstantElementValue.class);
-        result.addTestSuite(TestBooleanConstantElementValue.class);
-        result.addTestSuite(TestStringConstantElementValue.class);
-        result.addTestSuite(TestEnumElementValue.class);
-        result.addTestSuite(TestAnnotation.class);
-        result.addTestSuite(TestRuntimeVisibleAnnotations_attribute.class);
-        result.addTestSuite(TestRuntimeInvisibleAnnotations_attribute.class);
-        result.addTestSuite(TestParameter.class);
-        result.addTestSuite(TestRuntimeVisibleParameterAnnotations_attribute.class);
-        result.addTestSuite(TestRuntimeInvisibleParameterAnnotations_attribute.class);
+import com.jeantessier.classreader.*;
+import com.jeantessier.classreader.ConstantPoolEntry;
 
-        return result;
+public class EnumElementValue extends ElementValue implements com.jeantessier.classreader.EnumElementValue {
+    private int typeNameIndex;
+    private int constNameIndex;
+
+    public EnumElementValue(Classfile classfile, DataInput in) throws IOException {
+        super(classfile);
+
+        typeNameIndex = in.readUnsignedShort();
+        Logger.getLogger(getClass()).debug("Type name index: " + typeNameIndex);
+
+        constNameIndex = in.readUnsignedShort();
+        Logger.getLogger(getClass()).debug("Const name index: " + constNameIndex);
+    }
+
+    public int getTypeNameIndex() {
+        return typeNameIndex;
+    }
+
+    public ConstantPoolEntry getRawTypeName() {
+        return getClassfile().getConstantPool().get(getTypeNameIndex());
+    }
+
+    public String getTypeName() {
+        return ((String_info) getRawTypeName()).getValue();
+    }
+
+    public int getConstNameIndex() {
+        return constNameIndex;
+    }
+
+    public ConstantPoolEntry getRawConstName() {
+        return getClassfile().getConstantPool().get(getConstNameIndex());
+    }
+
+    public String getConstName() {
+        return ((String_info) getRawConstName()).getValue();
+    }
+
+    public char getTag() {
+        return 'e';
+    }
+
+    public void accept(Visitor visitor) {
+        //To change body of implemented methods use File | Settings | File Templates.
     }
 }
---
+++ DependencyFinder/tests/com/jeantessier/classreader/impl/TestEnumElementValue.java
@@ -0,0 +1,101 @@
+/*
+ *  Copyright (c) 2001-2008, Jean Tessier
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *      * Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *
+ *      * Redistributions in binary form must reproduce the above copyright
+ *        notice, this list of conditions and the following disclaimer in the
+ *        documentation and/or other materials provided with the distribution.
+ *
+ *      * Neither the name of Jean Tessier nor the names of his contributors
+ *        may be used to endorse or promote products derived from this software
+ *        without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ *  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ *  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.jeantessier.classreader.impl;
+
+import org.jmock.*;
+
+import com.jeantessier.classreader.*;
+
+public class TestEnumElementValue extends TestAnnotationsBase {
+    private static final int TYPE_NAME_INDEX = 2;
+    private static final int CONST_NAME_INDEX = 3;
+
+    private EnumElementValue sut;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        expectReadU2(TYPE_NAME_INDEX);
+        expectReadU2(CONST_NAME_INDEX);
+
+        sut = new EnumElementValue(mockClassfile, mockIn);
+    }
+
+    public void testGetTypeName() {
+        final String expectedValue = "abc";
+        final ConstantPool mockConstantPool = mock(ConstantPool.class);
+        final String_info mockString_info = mock(String_info.class);
+
+        checking(new Expectations() {{
+            one (mockClassfile).getConstantPool();
+                will(returnValue(mockConstantPool));
+            one (mockConstantPool).get(TYPE_NAME_INDEX);
+                will(returnValue(mockString_info));
+            one (mockString_info).getValue();
+                will(returnValue(expectedValue));
+        }});
+
+        assertEquals(expectedValue, sut.getTypeName());
+    }
+
+    public void testGetConstName() {
+        final String expectedValue = "abc";
+        final ConstantPool mockConstantPool = mock(ConstantPool.class);
+        final String_info mockString_info = mock(String_info.class);
+
+        checking(new Expectations() {{
+            one (mockClassfile).getConstantPool();
+                will(returnValue(mockConstantPool));
+            one (mockConstantPool).get(CONST_NAME_INDEX);
+                will(returnValue(mockString_info));
+            one (mockString_info).getValue();
+                will(returnValue(expectedValue));
+        }});
+
+        assertEquals(expectedValue, sut.getConstName());
+    }
+
+    public void testGetTag() {
+        assertEquals('e', sut.getTag());
+    }
+
+    public void testAccept() {
+        final Visitor mockVisitor = mock(Visitor.class);
+
+        checking(new Expectations() {{
+//            one (mockVisitor).visitEnumElementValue(sut);
+        }});
+
+        sut.accept(mockVisitor);
+    }
+}
