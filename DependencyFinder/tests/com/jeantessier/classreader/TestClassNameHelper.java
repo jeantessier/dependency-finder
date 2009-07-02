62015cfe9f73be87ee599c4aa64a8994c775abd8
jeantessier <jeantessier@1c2012b4-aa10-0410-bfa9-9e87fac9e5c7>
2009-07-02 21:40:10 +0000

    Fix test that was calling the wrong method
    git-svn-id: https://depfind.svn.sourceforge.net/svnroot/depfind/trunk@1942 1c2012b4-aa10-0410-bfa9-9e87fac9e5c7

--- DependencyFinder/tests/com/jeantessier/classreader/TestClassNameHelper.java
+++ DependencyFinder/tests/com/jeantessier/classreader/TestClassNameHelper.java
@@ -36,9 +36,9 @@ import junit.framework.*;
 
 public class TestClassNameHelper extends TestCase {
     public void testPath2ClassName() {
-        assertEquals("I", ClassNameHelper.convertClassName("I"));
-        assertEquals("L", ClassNameHelper.convertClassName("L"));
-        assertEquals("V", ClassNameHelper.convertClassName("V"));
+        assertEquals("I", ClassNameHelper.path2ClassName("I"));
+        assertEquals("L", ClassNameHelper.path2ClassName("L"));
+        assertEquals("V", ClassNameHelper.path2ClassName("V"));
         assertEquals("List", ClassNameHelper.path2ClassName("List"));
         assertEquals("package.Class", ClassNameHelper.path2ClassName("package/Class"));
     }
