cdf435971667a15e4b9aff8155079de6c7dafdcd
Jean Tessier <jean@jeantessier.com>
2010-11-28 18:16:28 -0800

    Fix broken escaping of XML characters.

--- DependencyFinder/src/com/jeantessier/classreader/XMLPrinter.java
+++ DependencyFinder/src/com/jeantessier/classreader/XMLPrinter.java
@@ -32,19 +32,17 @@
 
 package com.jeantessier.classreader;
 
-import java.io.*;
-import java.util.*;
+import com.google.common.annotations.VisibleForTesting;
+import com.jeantessier.text.Hex;
 
-import org.apache.oro.text.perl.*;
-
-import com.jeantessier.text.*;
+import java.io.PrintWriter;
+import java.util.Collection;
 
 public class XMLPrinter extends Printer {
     public static final String DEFAULT_ENCODING   = "utf-8";
     public static final String DEFAULT_DTD_PREFIX = "http://depfind.sourceforge.net/dtd";
 
     private static final BitFormat format = new BitFormat(16);
-    private static final Perl5Util perl = new Perl5Util();
 
     private boolean top = true;
 
@@ -1008,13 +1006,33 @@ public class XMLPrinter extends Printer {
         }
     }
 
-    private String escapeXMLCharacters(String text) {
-        String result = text;
+    @VisibleForTesting
+    String escapeXMLCharacters(String text) {
+        StringBuilder result = new StringBuilder();
+        boolean containsControlCharacters = false;
 
-        result = perl.substitute("s/&/&amp;/g", result);
-        result = perl.substitute("s/</&lt;/g", result);
-        result = perl.substitute("s/>/&gt;/g", result);
+        for (int i=0; i<text.length(); i++) {
+            char c = text.charAt(i);
+            if (c == '&') {
+                result.append("&amp;");
+            } else if (c == '<') {
+                result.append("&lt;");
+            } else if (c == '>') {
+                result.append("&gt;");
+            } else if (Character.isISOControl(c) || c > 0x9F) {
+                containsControlCharacters = true;
+                result.append("&#x");
+                result.append(Integer.toString(c, 16).toUpperCase());
+                result.append(";");
+            } else {
+                result.append(c);
+            }
+        }
 
-        return result;
+        if (containsControlCharacters) {
+            return "<![CDATA[" + result.toString() + "]]>";
+        } else {
+            return result.toString();
+        }
     }
 }
