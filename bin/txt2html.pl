04e7581e91955279f1baa9c1e58aa300b87ab851
Jean Tessier <jean@jeantessier.com>
2023-04-10 11:56:58 -0700

    Update copyright notice

--- bin/txt2html.pl
+++ bin/txt2html.pl
@@ -1,7 +1,7 @@
 #!/usr/local/bin/perl -n
 
 #   
-#   Copyright (c) 2001-2009, Jean Tessier
+#   Copyright (c) 2001-2023, Jean Tessier
 #   All rights reserved.
 #   
 #   Redistribution and use in source and binary forms, with or without
