88a0008254b0b5236b903af55fe95641c116c5e3
Jean Tessier <jean@jeantessier.com>
2023-12-07 02:26:38 -0800

    Populate JAR manifest metadata

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -17,6 +17,33 @@ dependencies {
 }
 
 java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+tasks.jar {
+    manifest {
+        attributes(
+                "Specification-Vendor" to "Jean Tessier",
+                "Specification-Title" to "Dependency Finder",
+                "Specification-Version" to archiveVersion,
+//                "Specification-Date" to NOW,
+                "Implementation-Vendor" to "Jean Tessier",
+                "Implementation-Title" to "Dependency Finder",
+                "Implementation-Version" to archiveVersion,
+//                "Implementation-Date" to NOW,
+                "Implementation-URL" to "https://depfind.sourceforge.io/",
+                "Copyright-Holder" to "Jean Tessier",
+                "Copyright-Date" to "2001-2023",
+                "Compiler-Vendor" to System.getProperty("java.vendor"),
+                "Compiler-Title" to System.getProperty("java.runtime.name"),
+                "Compiler-Version" to System.getProperty("java.version"),
+        )
+        attributes(mapOf("Java-Bean" to "True"), "com/jeantessier/dependencyfinder/Version.class")
+    }
+}
+
+java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(17))
     }
