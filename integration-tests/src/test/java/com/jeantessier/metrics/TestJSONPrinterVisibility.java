f7613391fb86a50b8dbfb98ccc1db5ea97f5b065
Jean Tessier <jean@jeantessier.com>
2024-06-21 01:50:02 -0700

    Use data-driven tests for metrics printers
    They cut down on code duplication.
    
    I tried to use the `Consumer<T>` functional interface to pass custon
    initialization code from the data, but the compiler couldn't coerce it
    to the `Object` that was expected.

--- integration-tests/src/test/java/com/jeantessier/metrics/TestJSONPrinterVisibility.java
+++ integration-tests/src/test/java/com/jeantessier/metrics/TestJSONPrinterVisibility.java
@@ -1,68 +1,58 @@
 package com.jeantessier.metrics;
 
 import org.junit.*;
+import org.junit.runner.*;
+import org.junit.runners.*;
 
 import java.io.*;
 import java.nio.file.*;
 
 import static org.junit.Assert.*;
+import static org.junit.runners.Parameterized.*;
 
+@RunWith(Parameterized.class)
 public class TestJSONPrinterVisibility extends TestPrinterVisibilityBase {
-    @Test
-    public void testStandardReport() throws IOException {
-        // Given
-        var buffer = new StringWriter();
-        var out = new PrintWriter(buffer);
-
-        // and
-        var printer = new JSONPrinter(out, configuration);
-
-        // When
-        printer.visitMetrics(projectMetrics);
-        out.println();
-
-        // Then
-        var expectedReport = Files.readString(REPORTS_DIR.resolve("martin.metrics.json"));
-        var actualReport = buffer.toString();
-        assertEquals(expectedReport, actualReport);
+    @Parameters(name="generate a report with {0}")
+    public static Object[][] data() {
+        return new Object[][] {
+                {"standard options", JSONPrinter.class, false, false, "martin.metrics.json"},
+                {"expand option", JSONPrinter.class, true, false, "martin.metrics.expand.json"},
+                {"show-empty-metrics option", JSONPrinter.class, false, true, "martin.metrics.show-empty-metrics.json"},
+        };
     }
 
+    @Parameter(0)
+    public String variation;
+
+    @Parameter(1)
+    public Class<? extends Printer> printerClass;
+
+    @Parameter(2)
+    public boolean expandCollectionMeasurements;
+
+    @Parameter(3)
+    public boolean showEmptyMetrics;
+
+    @Parameter(4)
+    public String expectedOutput;
+
     @Test
-    public void testExpandCollectionMeasurements() throws IOException {
+    public void generateReportAndCompareToFile() throws Exception {
         // Given
         var buffer = new StringWriter();
         var out = new PrintWriter(buffer);
 
         // and
-        var printer = new JSONPrinter(out, configuration);
-        printer.setExpandCollectionMeasurements(true);
+        var printer = printerClass.getConstructor(out.getClass(), configuration.getClass()).newInstance(out, configuration);
+        printer.setExpandCollectionMeasurements(expandCollectionMeasurements);
+        printer.setShowEmptyMetrics(showEmptyMetrics);
 
         // When
         printer.visitMetrics(projectMetrics);
         out.println();
 
         // Then
-        var expectedReport = Files.readString(REPORTS_DIR.resolve("martin.metrics.expand.json"));
-        var actualReport = buffer.toString();
-        assertEquals(expectedReport, actualReport);
-    }
-
-    @Test
-    public void testShowEmptyMetrics() throws IOException {
-        // Given
-        var buffer = new StringWriter();
-        var out = new PrintWriter(buffer);
-
-        // and
-        var printer = new JSONPrinter(out, configuration);
-        printer.setShowEmptyMetrics(true);
-
-        // When
-        printer.visitMetrics(projectMetrics);
-        out.println();
-
-        // Then
-        var expectedReport = Files.readString(REPORTS_DIR.resolve("martin.metrics.show-empty-metrics.json"));
+        var expectedReport = Files.readString(REPORTS_DIR.resolve(expectedOutput));
         var actualReport = buffer.toString();
         assertEquals(expectedReport, actualReport);
     }
