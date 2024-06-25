283a64b8c5f2479b686dc25c1cec473fc6d91129
Jean Tessier <jean@jeantessier.com>
2024-06-24 23:15:34 -0700

    Add tests for XML metrics printer

---
+++ integration-tests/generate_expected_outputs.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+# Generates expected outputs from tools for use in integration tests.
+
+# Run this script from the `integration-tests` directory:
+#     ./generate_expected_outputs.sh
+
+# To generate outputs using a different version of Dependency Finder,
+# set $DEPENDENCYFINDER_HOME environment variable to the location of
+# this other version.
+
+readonly BINDIR=$DEPENDENCYFINDER_HOME/bin
+readonly ETCDIR=$DEPENDENCYFINDER_HOME/etc
+
+readonly OUTPUTDIR=src/test/resources
+
+# OOMetrics JSON output
+echo "Generating OOMetrics JSON output"
+$BINDIR/OOMetrics -configuration $ETCDIR/MartinConfig.xml metrics/build/classes/java/main/{client,provider} -out $OUTPUTDIR/martin.metrics.json -json
+$BINDIR/OOMetrics -configuration $ETCDIR/MartinConfig.xml metrics/build/classes/java/main/{client,provider} -out $OUTPUTDIR/martin.metrics.expand.json -json -expand
+$BINDIR/OOMetrics -configuration $ETCDIR/MartinConfig.xml metrics/build/classes/java/main/{client,provider} -out $OUTPUTDIR/martin.metrics.show-empty-metrics.json -json -show-empty-metrics
+$BINDIR/OOMetrics -configuration $ETCDIR/MartinConfig.xml metrics/build/classes/java/main/{client,provider} -out $OUTPUTDIR/martin.metrics.show-hidden-measurements.json -json -show-hidden-measurements
+$BINDIR/OOMetrics -configuration $ETCDIR/MartinConfig.xml metrics/build/classes/java/main/{client,provider} -out $OUTPUTDIR/martin.metrics.expand.show-hidden-measurements.json -json -expand -show-hidden-measurements
+
+# OOMetrics YAML output
+echo "Generating OOMetrics YAML output"
+$BINDIR/OOMetrics -configuration $ETCDIR/MartinConfig.xml metrics/build/classes/java/main/{client,provider} -out $OUTPUTDIR/martin.metrics.yml -yml
+$BINDIR/OOMetrics -configuration $ETCDIR/MartinConfig.xml metrics/build/classes/java/main/{client,provider} -out $OUTPUTDIR/martin.metrics.expand.yml -yml -expand
+$BINDIR/OOMetrics -configuration $ETCDIR/MartinConfig.xml metrics/build/classes/java/main/{client,provider} -out $OUTPUTDIR/martin.metrics.show-empty-metrics.yml -yml -show-empty-metrics
+$BINDIR/OOMetrics -configuration $ETCDIR/MartinConfig.xml metrics/build/classes/java/main/{client,provider} -out $OUTPUTDIR/martin.metrics.show-hidden-measurements.yml -yml -show-hidden-measurements
+$BINDIR/OOMetrics -configuration $ETCDIR/MartinConfig.xml metrics/build/classes/java/main/{client,provider} -out $OUTPUTDIR/martin.metrics.expand.show-hidden-measurements.yml -yml -expand -show-hidden-measurements
+
+# OOMetrics XML output
+echo "Generating OOMetrics XML output"
+$BINDIR/OOMetrics -configuration $ETCDIR/MartinConfig.xml metrics/build/classes/java/main/{client,provider} -out $OUTPUTDIR/martin.metrics.xml -xml
+$BINDIR/OOMetrics -configuration $ETCDIR/MartinConfig.xml metrics/build/classes/java/main/{client,provider} -out $OUTPUTDIR/martin.metrics.expand.xml -xml -expand
+$BINDIR/OOMetrics -configuration $ETCDIR/MartinConfig.xml metrics/build/classes/java/main/{client,provider} -out $OUTPUTDIR/martin.metrics.show-empty-metrics.xml -xml -show-empty-metrics
+$BINDIR/OOMetrics -configuration $ETCDIR/MartinConfig.xml metrics/build/classes/java/main/{client,provider} -out $OUTPUTDIR/martin.metrics.show-hidden-measurements.xml -xml -show-hidden-measurements
+$BINDIR/OOMetrics -configuration $ETCDIR/MartinConfig.xml metrics/build/classes/java/main/{client,provider} -out $OUTPUTDIR/martin.metrics.expand.show-hidden-measurements.xml -xml -expand -show-hidden-measurements
+
+# OOMetrics text output
+for section in project groups classes methods
+do
+    echo "Generating OOMetrics text output for $section"
+    $BINDIR/OOMetrics -configuration $ETCDIR/MartinConfig.xml metrics/build/classes/java/main/{client,provider} -out $OUTPUTDIR/martin.metrics_$section.txt -txt -$section
+    $BINDIR/OOMetrics -configuration $ETCDIR/MartinConfig.xml metrics/build/classes/java/main/{client,provider} -out $OUTPUTDIR/martin.metrics.expand_$section.txt -txt -$section -expand
+    $BINDIR/OOMetrics -configuration $ETCDIR/MartinConfig.xml metrics/build/classes/java/main/{client,provider} -out $OUTPUTDIR/martin.metrics.show-empty-metrics_$section.txt -txt -$section -show-empty-metrics
+    $BINDIR/OOMetrics -configuration $ETCDIR/MartinConfig.xml metrics/build/classes/java/main/{client,provider} -out $OUTPUTDIR/martin.metrics.show-hidden-measurements_$section.txt -txt -$section -show-hidden-measurements
+    $BINDIR/OOMetrics -configuration $ETCDIR/MartinConfig.xml metrics/build/classes/java/main/{client,provider} -out $OUTPUTDIR/martin.metrics.expand.show-hidden-measurements_$section.txt -txt -$section -expand -show-hidden-measurements
+done
+
+# OOMetrics CSV output
+echo "Generating OOMetrics CSV output"
+$BINDIR/OOMetrics -configuration $ETCDIR/MartinConfig.xml metrics/build/classes/java/main/{client,provider} -out $OUTPUTDIR/martin.metrics -csv
+$BINDIR/OOMetrics -configuration $ETCDIR/MartinConfig.xml metrics/build/classes/java/main/{client,provider} -out $OUTPUTDIR/martin.metrics.expand -csv -expand
+$BINDIR/OOMetrics -configuration $ETCDIR/MartinConfig.xml metrics/build/classes/java/main/{client,provider} -out $OUTPUTDIR/martin.metrics.show-empty-metrics -csv -show-empty-metrics
+$BINDIR/OOMetrics -configuration $ETCDIR/MartinConfig.xml metrics/build/classes/java/main/{client,provider} -out $OUTPUTDIR/martin.metrics.show-hidden-measurements -csv -show-hidden-measurements
+$BINDIR/OOMetrics -configuration $ETCDIR/MartinConfig.xml metrics/build/classes/java/main/{client,provider} -out $OUTPUTDIR/martin.metrics.expand.show-hidden-measurements -csv -expand -show-hidden-measurements
---
+++ integration-tests/src/test/java/com/jeantessier/metrics/TestTextPrinterVisibility.java
@@ -0,0 +1,142 @@
+package com.jeantessier.metrics;
+
+import org.junit.*;
+import org.junit.runner.*;
+import org.junit.runners.*;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+import static java.util.stream.Collectors.*;
+import static org.junit.Assert.*;
+import static org.junit.runners.Parameterized.*;
+
+@RunWith(Parameterized.class)
+public class TestTextPrinterVisibility extends TestPrinterVisibilityBase {
+    @Parameters(name="generate a report with {0}")
+    public static Object[][] data() {
+        return new Object[][] {
+                {"standard options", TextPrinter.class, false, false, false, "martin.metrics"},
+                {"expand option", TextPrinter.class, true, false, false, "martin.metrics.expand"},
+                {"show-empty-metrics option", TextPrinter.class, false, true, false, "martin.metrics.show-empty-metrics"},
+                {"show-hidden-measurements option", TextPrinter.class, false, false, true, "martin.metrics.show-hidden-measurements"},
+                {"expand and show-hidden-measurements options", TextPrinter.class, true, false, true, "martin.metrics.expand.show-hidden-measurements"},
+        };
+    }
+
+    @Parameter(0)
+    public String variation;
+
+    @Parameter(1)
+    public Class<? extends Printer> printerClass;
+
+    @Parameter(2)
+    public boolean expandCollectionMeasurements;
+
+    @Parameter(3)
+    public boolean showEmptyMetrics;
+
+    @Parameter(4)
+    public boolean showHiddenMeasurements;
+
+    @Parameter(5)
+    public String expectedOutput;
+
+    @Test
+    public void generateProjectReportAndCompareToFile() throws Exception {
+        // Given
+        var buffer = new StringWriter();
+        var out = new PrintWriter(buffer);
+
+        // and
+        var printer = createPrinter(out, configuration.getProjectMeasurements());
+
+        // When
+        projectMetrics.stream()
+                .sorted(new MetricsComparator("name"))
+                .forEach(printer::visitMetrics);
+
+        // Then
+        var expectedReport = readExpectedReport("project");
+        var actualReport = buffer.toString();
+        assertEquals(expectedReport, actualReport);
+    }
+
+    @Test
+    public void generateGroupReportAndCompareToFile() throws Exception {
+        // Given
+        var buffer = new StringWriter();
+        var out = new PrintWriter(buffer);
+
+        // and
+        var printer = createPrinter(out, configuration.getGroupMeasurements());
+
+        // When
+        groupMetrics.stream()
+                .sorted(new MetricsComparator("name"))
+                .forEach(printer::visitMetrics);
+
+        // Then
+        var expectedReport = readExpectedReport("groups");
+        var actualReport = buffer.toString();
+        assertEquals(expectedReport, actualReport);
+    }
+
+    @Test
+    public void generateClassReportAndCompareToFile() throws Exception {
+        // Given
+        var buffer = new StringWriter();
+        var out = new PrintWriter(buffer);
+
+        // and
+        var printer = createPrinter(out, configuration.getClassMeasurements());
+
+        // When
+        classMetrics.stream()
+                .sorted(new MetricsComparator("name"))
+                .forEach(printer::visitMetrics);
+
+        // Then
+        var expectedReport = readExpectedReport("classes");
+        var actualReport = buffer.toString();
+        assertEquals(expectedReport, actualReport);
+    }
+
+    @Test
+    public void generateMethodReportAndCompareToFile() throws Exception {
+        // Given
+        var buffer = new StringWriter();
+        var out = new PrintWriter(buffer);
+
+        // and
+        var printer = createPrinter(out, configuration.getMethodMeasurements());
+
+        // When
+        methodMetrics.stream()
+                .sorted(new MetricsComparator("name"))
+                .forEach(printer::visitMetrics);
+
+        // Then
+        var expectedReport = readExpectedReport("methods");
+        var actualReport = buffer.toString();
+        assertEquals(expectedReport, actualReport);
+    }
+
+    private Printer createPrinter(PrintWriter out, List<MeasurementDescriptor> descriptors) throws Exception {
+        var printer = printerClass.getConstructor(out.getClass(), List.class).newInstance(out, descriptors);
+        printer.setExpandCollectionMeasurements(expandCollectionMeasurements);
+        printer.setShowEmptyMetrics(showEmptyMetrics);
+        printer.setShowHiddenMeasurements(showHiddenMeasurements);
+
+        return printer;
+    }
+
+    private String readExpectedReport(String section) throws IOException {
+        try (var stream = Files.lines(REPORTS_DIR.resolve(expectedOutput + "_" + section + ".txt"))) {
+            return stream
+                    .skip(2) // The first two lines come from OOMetrics, not the printer
+                    .collect(joining(System.getProperty("line.separator", "\n")));
+        }
+    }
+}
