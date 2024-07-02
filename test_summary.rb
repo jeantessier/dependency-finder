c2b43c16c8b7a48a6b07ee4a6633f460d8b8bbbc
Jean Tessier <jean@jeantessier.com>
2024-07-01 22:12:03 -0700

    Refactor test summary script

--- test_summary.rb
+++ test_summary.rb
@@ -6,44 +6,23 @@ puts "|------------|:------:|:-----:|:-------:|:--------:|:------:|"
 Dir.glob('*/build/test-results/test/TEST-*.xml')
    .group_by {|name| name.split(%'/', 2).first}
    .each do |group, test_results|
-      tests = test_results.map do |name|
+      docs = test_results.map do |name|
         File.open(name) {|f| Nokogiri::XML f}
       end
-                 .map {|doc| doc.xpath "//@tests"}
-                 .flatten
-                 .map(&:value)
-                 .map(&:to_i)
-                 .sum
 
-      skipped = test_results.map do |name|
-        File.open(name) {|f| Nokogiri::XML f}
+      counts = Hash.new(0)
+
+      [
+        :tests,
+        :skipped,
+        :failures,
+        :errors,
+      ].each do |attr|
+            counts[attr] = docs.map {|doc| doc.xpath "//@#{attr}"}.flatten.map(&:value).map(&:to_i).sum
       end
-                          .map {|doc| doc.xpath "//@skipped"}
-                          .flatten
-                          .map(&:value)
-                          .map(&:to_i)
-                          .sum
 
-      failures = test_results.map do |name|
-        File.open(name) {|f| Nokogiri::XML f}
-      end
-                            .map {|doc| doc.xpath "//@failures"}
-                            .flatten
-                            .map(&:value)
-                            .map(&:to_i)
-                            .sum
+      status = counts[:failures] == 0 && counts[:errors] == 0
 
-      errors = test_results.map do |name|
-        File.open(name) {|f| Nokogiri::XML f}
-      end
-                            .map {|doc| doc.xpath "//@errors"}
-                            .flatten
-                            .map(&:value)
-                            .map(&:to_i)
-                            .sum
-
-      status = failures == 0 && errors == 0
-
-      puts "| #{group} | #{status ? ":white_check_mark:" : ":x:"} | #{tests} | #{skipped} | #{failures} | #{errors} |"
+      puts "| #{group} | #{status ? ":white_check_mark:" : ":x:"} | #{counts[:tests]} | #{counts[:skipped]} | #{counts[:failures]} | #{counts[:errors]} |"
     end
 
