1f51317242a762953f2a247318b011c6c379aeeb
Jean Tessier <jean@jeantessier.com>
2024-07-01 19:16:26 -0700

    Write table with count breakdowns

--- test_summary.rb
+++ test_summary.rb
@@ -1,12 +1,49 @@
 require 'nokogiri'
 
-tests = Dir.glob('*/build/test-results/test/TEST-*.xml').map do |name|
-  File.open(name) {|f| Nokogiri::XML f}
-end
-           .map {|doc| doc.xpath "//@tests"}
-           .flatten
-           .map(&:value)
-           .map(&:to_i)
-           .sum
+puts "| Subproject | Status | Tests | Skipped | Failures | Errors |"
+puts "|------------|:------:|:-----:|:-------:|:--------:|:------:|"
 
-puts "There were #{tests} test(s)."
+Dir.glob('*/build/test-results/test/TEST-*.xml')
+   .group_by {|name| name.split(%'/', 2).first}
+   .each do |group, test_results|
+      tests = test_results.map do |name|
+        File.open(name) {|f| Nokogiri::XML f}
+      end
+                 .map {|doc| doc.xpath "//@tests"}
+                 .flatten
+                 .map(&:value)
+                 .map(&:to_i)
+                 .sum
+
+      skipped = test_results.map do |name|
+        File.open(name) {|f| Nokogiri::XML f}
+      end
+                          .map {|doc| doc.xpath "//@skipped"}
+                          .flatten
+                          .map(&:value)
+                          .map(&:to_i)
+                          .sum
+
+      failures = test_results.map do |name|
+        File.open(name) {|f| Nokogiri::XML f}
+      end
+                            .map {|doc| doc.xpath "//@failures"}
+                            .flatten
+                            .map(&:value)
+                            .map(&:to_i)
+                            .sum
+
+      errors = test_results.map do |name|
+        File.open(name) {|f| Nokogiri::XML f}
+      end
+                            .map {|doc| doc.xpath "//@errors"}
+                            .flatten
+                            .map(&:value)
+                            .map(&:to_i)
+                            .sum
+
+      status = failures == 0 && errors == 0
+
+      puts "| #{group} | #{status ? ":white_check_mark:" : ":x:"} | #{tests} | #{skipped} | #{failures} | #{errors} |"
+    end
+
